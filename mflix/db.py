2c9732c8ec70fab723221fedd1b351ee256e4e14
Danish Parvez <danish.parvez19@gmail.com>
2018-12-04 13:13:12 +0530

    Implements User preference updates

--- mflix/db.py
+++ mflix/db.py
@@ -495,7 +495,7 @@ def update_prefs(email, prefs):
 
         # TODO: User preferences
         # Use the data in "prefs" to update the user's preferences.
-        response = db.users.update_one({"some_field": "some_value"}, {"$set": {"some_other_field": "some_other_value"}})
+        response = db.users.update_one({"email": email}, {"$set": {"preferences": prefs}})
         if response.matched_count == 0:
             return {'error': 'no user found'}
         else:
