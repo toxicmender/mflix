dea5ecd707542433aa328bef26f95fdc43b1e5e2
Danish Parvez <danish.parvez19@gmail.com>
2018-12-07 05:22:35 +0530

    Implements comment deletion by user for their own comments

--- mflix/db.py
+++ mflix/db.py
@@ -361,7 +361,7 @@ def delete_comment(comment_id, user_email):
     # TODO: Delete Comments
     # Use the user_email and comment_id to delete the proper comment.
     response = db.comments.delete_one(
-        {"_id": ObjectId(comment_id)}
+        {"_id": ObjectId(comment_id), "email": user_email}
     )
     return response
 
